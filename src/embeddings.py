42aadf17e9e327c4cee8b035242755fb72be9086
Erik Luu <eeluu19@gmail.com>
2024-05-26 11:35:38 -0700

    need another implementation for softcos, working with word2vec now

--- src/embeddings.py
+++ src/embeddings.py
@@ -3,6 +3,9 @@ import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModel, logging
 from tqdm import tqdm
 from typing import Any, List, Tuple
+import gensim.downloader as api
+from gensim.models import Word2Vec
+import numpy as np
 
 logging.set_verbosity_error()
 
@@ -17,7 +20,6 @@ def initialize_sentence_transformer_model(model_id: str) -> Tuple[Any, Any, int]
     max_length = tokenizer.model_max_length
     return tokenizer, model, max_length
 
-
 def initialize_nomic_model() -> Tuple[Any, Any, int]:
     print("Initializing Nomic Model")
     tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
@@ -26,7 +28,6 @@ def initialize_nomic_model() -> Tuple[Any, Any, int]:
     max_length = tokenizer.model_max_length
     return tokenizer, model, max_length
 
-
 def initialize_google_bert_model() -> Tuple[Any, Any, int]:
     print("Initializing Google BERT Model")
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -34,7 +35,6 @@ def initialize_google_bert_model() -> Tuple[Any, Any, int]:
     model.eval()
     max_length = tokenizer.model_max_length
     return tokenizer, model, max_length
-
 
 def initialize_specter_base_model() -> Tuple[Any, Any, int]:
     print("Initializing AllenAI Specter Model")
@@ -45,7 +45,6 @@ def initialize_specter_base_model() -> Tuple[Any, Any, int]:
     max_length = 512
     return tokenizer, model, max_length
 
-
 def initialize_llama_model() -> Tuple[Any, Any, int]:
     print("Initializing Llama Model")
     tokenizer = AutoTokenizer.from_pretrained("McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp", trust_remote_code=True)
@@ -53,6 +52,11 @@ def initialize_llama_model() -> Tuple[Any, Any, int]:
     model.eval()
     max_length = tokenizer.model_max_length
     return tokenizer, model, max_length 
+
+def initialize_word2vec_model() -> Word2Vec:
+    print("Initializing Word2Vec Model")
+    model = api.load("word2vec-google-news-300")
+    return model
 
 # -----------------
 # Mean Pooling Functions
@@ -71,9 +75,6 @@ def mean_pooling(model_output, attention_mask: torch.Tensor) -> torch.Tensor:
 def split_into_chunks(text: str, tokenizer, max_length: int) -> List[str]:
     """Split text into chunks that fit within the model's max context length."""
     tokens = tokenizer.encode(text, add_special_tokens=False)
-    # print(f"Original token length: {len(tokens)}")  # Debugging: print original token length
-    
-    # Split tokens into chunks of max_length - 2 to account for special tokens
     chunks = []
     for i in range(0, len(tokens), max_length - 2):
         chunk = tokens[i:i + max_length - 2]
@@ -82,25 +83,24 @@ def split_into_chunks(text: str, tokenizer, max_length: int) -> List[str]:
         if len(chunk_tokens) <= max_length:
             chunks.append(chunk_text)
         else:
-            # Further split if the chunk with special tokens exceeds max_length
             sub_chunks = [chunk[j:j + max_length - 2] for j in range(0, len(chunk), max_length - 2)]
             for sub_chunk in sub_chunks:
                 sub_chunk_text = tokenizer.decode(sub_chunk, skip_special_tokens=True)
                 chunks.append(sub_chunk_text)
-    
-    # Print lengths of chunks for debugging
-    # for chunk_text in chunks:
-    #     chunk_tokens = tokenizer.encode(chunk_text, add_special_tokens=True)
-    #     print(f"Chunk token length with special tokens: {len(chunk_tokens)}")  # Debugging: print chunk token length with special tokens
-    
     return chunks
-
 
 def aggregate_embeddings(embeddings: List[torch.Tensor]) -> torch.Tensor:
     """Aggregate embeddings using mean pooling."""
     stacked_embeddings = torch.stack(embeddings, dim=0)
     aggregated_embedding = torch.mean(stacked_embeddings, dim=0)
     return F.normalize(aggregated_embedding, p=2, dim=0)
+
+def document_to_vector(model, doc):
+    """Convert a document into a vector by averaging its word vectors."""
+    word_vectors = [model[word] for word in doc if word in model]
+    if not word_vectors:  # Handle the case where none of the words are in the model
+        return np.zeros(model.vector_size)
+    return np.mean(word_vectors, axis=0)
 
 # -----------------
 # Embedding Functions
@@ -113,7 +113,6 @@ def get_embeddings(input: List[str], tokenizer: type[Any], model: type[AutoModel
         chunks = split_into_chunks(document, tokenizer, max_length)
         chunk_embeddings = []
         for chunk in chunks:
-            # print(f"Processing chunk: {chunk[:50]}... (length: {len(chunk)})")
             encoded_input = tokenizer(chunk, padding=True, truncation=True, return_tensors='pt', max_length=max_length)
             with torch.no_grad():
                 model_output = model(**encoded_input)
@@ -123,6 +122,10 @@ def get_embeddings(input: List[str], tokenizer: type[Any], model: type[AutoModel
         document_embeddings.append(document_embedding)
     return torch.stack(document_embeddings)
 
+def get_word2vec_embeddings(input: List[List[str]], model: Word2Vec) -> torch.Tensor:
+    """Get Word2Vec embeddings for a list of documents."""
+    document_embeddings = [document_to_vector(model, doc) for doc in input]
+    return torch.tensor(document_embeddings)
 
 def batch_embeddings(input: List[str], tokenizer: type[AutoTokenizer], model: type[AutoModel], batch_size: int = 32, save_path: str = "", max_length: int = 512) -> torch.Tensor:
     """Get embeddings for a list of documents in batches."""
@@ -132,6 +135,20 @@ def batch_embeddings(input: List[str], tokenizer: type[AutoTokenizer], model: ty
         batch = input[i: i + batch_size]
         batch_embeddings = get_embeddings(batch, tokenizer, model, max_length)
         pbar.set_description(f"Processing batch: {batch[0][:20]}...")
+        embeddings.append(batch_embeddings)
+        if save_path:
+            torch.save(torch.cat(embeddings, dim=0), save_path)
+    embeddings = torch.cat(embeddings, dim=0)
+    return embeddings
+
+def batch_word2vec_embeddings(input: List[List[str]], model: Word2Vec, batch_size: int = 32, save_path: str = "") -> torch.Tensor:
+    """Get Word2Vec embeddings for a list of documents in batches."""
+    embeddings = []
+    pbar = tqdm(range(0, len(input), batch_size))
+    for i in pbar:
+        batch = input[i: i + batch_size]
+        batch_embeddings = get_word2vec_embeddings(batch, model)
+        pbar.set_description(f"Processing batch: {' '.join(batch[0][:3])}...")
         embeddings.append(batch_embeddings)
         if save_path:
             torch.save(torch.cat(embeddings, dim=0), save_path)
@@ -158,38 +175,43 @@ def get_model_and_pooling_func(model_name: str) -> Tuple[Any, Any, int]:
         tokenizer, model, max_length = initialize_specter_base_model()
     elif model_name == "llama":
         tokenizer, model, max_length = initialize_llama_model()
+    elif model_name == "word2vec":
+        model = initialize_word2vec_model()
+        return None, model, None  # Word2Vec doesn't use tokenizer and max_length
     else:
         raise ValueError(f"Model name {model_name} is not supported.")
     
     return tokenizer, model, max_length
 
-
-def process_embeddings(input: List[str], model_name: str, batch_size: int = 32, save_path: str = "") -> torch.Tensor:
+def process_embeddings(input: List[Any], model_name: str, batch_size: int = 32, save_path: str = "") -> torch.Tensor:
     """Process embeddings based on the specified model name."""
     tokenizer, model, max_length = get_model_and_pooling_func(model_name)
-    embeddings = batch_embeddings(input, tokenizer, model, batch_size, save_path, max_length)
+    if model_name == "word2vec":
+        embeddings = batch_word2vec_embeddings(input, model, batch_size, save_path)
+    else:
+        embeddings = batch_embeddings(input, tokenizer, model, batch_size, save_path, max_length)
     return embeddings
-
 
 if __name__ == "__main__":
     strings = ["Hello, my name is Erik.", "What is that song called?", "Tell me the name of that song.", "What year was that song made?"]
+    tokenized_strings = [string.lower().split() for string in strings]
     
-    nomic_embeddings = process_embeddings(strings, "nomic-embed-text-v1.5")
+    nomic_embeddings = process_embeddings(strings, "nomic")
     print("Nomic Model Embeddings:")
     print(nomic_embeddings.size())
     print(nomic_embeddings)
     
-    st_embeddings = process_embeddings(strings,  "all-MiniLM-L6-v2")
-    print("Sentence Transformer (all-mini) Embeddings:")
+    st_embeddings = process_embeddings(strings,  "minilm")
+    print("Sentence Transformer (MiniLM) Embeddings:")
     print(st_embeddings.size())
     print(st_embeddings)
 
-    st_embeddings = process_embeddings(strings,  "all-mpnet-base-v2")
-    print("Sentence Transformer (all-mpnet) Embeddings:")
+    st_embeddings = process_embeddings(strings,  "mpnet")
+    print("Sentence Transformer (MPNet) Embeddings:")
     print(st_embeddings.size())
     print(st_embeddings)
 
-    bert_embeddings = process_embeddings(strings, "bert_uncased")
+    bert_embeddings = process_embeddings(strings, "bert")
     print("Google BERT Model Embeddings:")
     print(bert_embeddings.size())
     print(bert_embeddings)
@@ -198,3 +220,8 @@ if __name__ == "__main__":
     print("AllenAI Specter Model Embeddings:")
     print(specter_embeddings.size())
     print(specter_embeddings)
+
+    word2vec_embeddings = process_embeddings(tokenized_strings, "word2vec")
+    print("Word2Vec Model Embeddings:")
+    print(word2vec_embeddings.size())
+    print(word2vec_embeddings)
--- src/similarity.py
+++ src/similarity.py
@@ -2,30 +2,54 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 
-def soft_cosine_similarity(batch0, batch1):
+def compute_feature_similarity(features):
+    features = features.t()  # Transpose the feature matrix to get features as rows
+    norm_features = F.normalize(features, p=2, dim=1)  # Normalize the feature vectors
+    feature_similarity = torch.mm(norm_features, norm_features.t())  # Compute the cosine similarity matrix
+    print(feature_similarity)
+    return feature_similarity
+
+
+def soft_cosine_similarity(batch0, batch1, feature_similarity):
+    assert feature_similarity.size(0) == batch0.size(1) and feature_similarity.size(1) == batch1.size(1), \
+        "Feature similarity matrix dimensions do not match the feature dimensions of the batches"
+    
     norm_batch0 = F.normalize(batch0, p=2, dim=1)
     norm_batch1 = F.normalize(batch1, p=2, dim=1)
-    return norm_batch0 @ norm_batch1.t()
+    
+    # Compute the soft cosine similarity
+    soft_cos_sim = torch.zeros(batch0.size(0), batch1.size(0), device=batch0.device)
+    for i in range(batch0.size(0)):
+        for j in range(batch1.size(0)):
+            # Compute the weighted inner product
+            sim = torch.matmul(norm_batch0[i], torch.matmul(feature_similarity, norm_batch1[j]))
+            soft_cos_sim[i, j] = sim
+            # Debugging: Print intermediate similarity value
+            # print(f"Soft Cosine Similarity [i={i}, j={j}]: {sim.item()}")
+
+    # min_sim = torch.min(soft_cos_sim)
+    # max_sim = torch.max(soft_cos_sim)
+    # soft_cos_sim = 2 * (soft_cos_sim - min_sim) / (max_sim - min_sim) - 1
+    
+    return soft_cos_sim
 
 
-def euclidean_similarity(batch0, batch1, sigma=1.0):
-    distances = torch.cdist(batch0, batch1, p=2)
-    similarities = torch.exp(-distances ** 2 / (2 * sigma ** 2))
-    return similarities
-
-
-def similarity_scores(batch0, batch1, metric, sigma=1.0):
+def similarity_scores(batch0, batch1, metric, feature_similarity=None, sigma=1.0):
     if metric == "cosine":
         return F.cosine_similarity(batch0.unsqueeze(1), batch1.unsqueeze(0), dim=2)
     elif metric == "euclidean":
-        return euclidean_similarity(batch0, batch1, sigma)
+        distances = torch.cdist(batch0, batch1, p=2)
+        similarities = torch.exp(-distances ** 2 / (2 * sigma ** 2))
+        return similarities
     elif metric == "soft_cosine":
-        return soft_cosine_similarity(batch0, batch1)
+        if feature_similarity is None:
+            raise ValueError("Feature similarity matrix must be provided for soft cosine similarity")
+        return soft_cosine_similarity(batch0, batch1, feature_similarity)
     else:
         raise ValueError(f"Unknown metric: {metric}")
 
 
-def batch_similarity_scores(matrix, metric="cosine", batch_size=256):
+def batch_similarity_scores(matrix, metric="cosine", feature_similarity=None, batch_size=256, sigma=1.0):
     n = matrix.size(0)
     similarity_matrix = None
 
@@ -37,39 +61,39 @@ def batch_similarity_scores(matrix, metric="cosine", batch_size=256):
             batch1 = matrix[j:j + batch_size]
 
             if batch_similarity_matrix is None:
-                batch_similarity_matrix = similarity_scores(batch0, batch1, metric)
+                batch_similarity_matrix = similarity_scores(batch0, batch1, metric, feature_similarity, sigma)
             else:
-                batch_batch_similarity_matrix = similarity_scores(batch0, batch1, metric)
+                batch_batch_similarity_matrix = similarity_scores(batch0, batch1, metric, feature_similarity, sigma)
                 batch_similarity_matrix = torch.cat((batch_similarity_matrix, batch_batch_similarity_matrix), dim=1)
 
         if similarity_matrix is None:
             similarity_matrix = batch_similarity_matrix
         else:
-            if similarity_matrix.size(1) != batch_similarity_matrix.size(1): # pyright: ignore
-                difference = similarity_matrix.size(1) - batch_similarity_matrix.size(1) # pyright: ignore
+            if similarity_matrix.size(1) != batch_similarity_matrix.size(1):
+                difference = similarity_matrix.size(1) - batch_similarity_matrix.size(1)
                 padding = (0, abs(difference)) if difference < 0 else (0, 0)
-                batch_similarity_matrix = F.pad(batch_similarity_matrix, padding) # pyright: ignore
+                batch_similarity_matrix = F.pad(batch_similarity_matrix, padding)
                 similarity_matrix = F.pad(similarity_matrix, (-difference, 0))
-            similarity_matrix = torch.cat((similarity_matrix, batch_similarity_matrix)) # pyright: ignore
+            similarity_matrix = torch.cat((similarity_matrix, batch_similarity_matrix))
 
     return similarity_matrix
 
 
-def get_all_similarities(embeddings):
+def get_all_similarities(embeddings, feature_similarity, sigma=1.0):
     cosine_similarity = batch_similarity_scores(embeddings, metric="cosine")
-    soft_cosine_similarity = batch_similarity_scores(embeddings, metric="soft_cosine")
-    euclidean_similarity = batch_similarity_scores(embeddings, metric="euclidean")
+    soft_cosine_similarity = batch_similarity_scores(embeddings, metric="soft_cosine", feature_similarity=feature_similarity)
+    euclidean_similarity = batch_similarity_scores(embeddings, metric="euclidean", sigma=sigma)
 
     return cosine_similarity, soft_cosine_similarity, euclidean_similarity
 
-# if __name__ == "__main__":
-#     matrix0 = torch.randn(100, 1000)
-#     matrix1 = torch.randn(100, 1000)
-#     sim_mat = similarity_scores(matrix0)
-#     batch_sim_mat = batch_similarity_scores(matrix0)
-#     assert torch.equal(sim_mat, batch_sim_mat) # pyright: ignore
 
-#     a = torch.tensor([[0.9041,  0.0196], [-0.3108, -2.4423], [-0.4821,  1.059]])
-#     print(a)
-#     print(similarity_scores(a, metric="euclidean"))
-#     print(similarity_scores(a, metric="cosine")) 
+document_embeddings = torch.randn(100, 300)  # Example document embeddings
+feature_similarity = compute_feature_similarity(document_embeddings)
+
+batch0 = document_embeddings[:10]  # First 10 samples
+batch1 = document_embeddings[10:20]  # Next 10 samples
+
+cosine_sim, soft_cos_sim, euclidean_sim = get_all_similarities(document_embeddings, feature_similarity)
+print("Cosine Similarity:\n", cosine_sim)
+print("Soft Cosine Similarity:\n", soft_cos_sim)
+print("Euclidean Similarity:\n", euclidean_sim)
